50f8302fa51ec949e3a8ffef590b70c6c0951edf
royder789 <154967771+royder789@users.noreply.github.com>
2026-02-17 14:42:54 +0530

    Add files via upload

---
+++ main/java/com/example/demo/SpringwebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebMvcApplication.class, args);
+	}
+
+}
