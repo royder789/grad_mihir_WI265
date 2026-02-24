1f274383b59d39a049e47bba75cda865245c4a35
royder789 <154967771+royder789@users.noreply.github.com>
2026-02-24 16:39:49 +0530

    Add CalculatorMain class with basic operations
    Implement a simple calculator with basic operations.

---
+++ AssignmentJenkinsDemoCalc/CalculatorMain.java
@@ -0,0 +1,17 @@
+public class CalculatorMain {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        int result1 = calc.add(10, 5);
+        int result2 = calc.subtract(10, 5);
+        int result3 = calc.multiply(10, 5);
+        int result4 = calc.divide(10, 5);
+
+        System.out.println("Addition: " + result1);
+        System.out.println("Subtraction: " + result2);
+        System.out.println("Multiplication: " + result3);
+        System.out.println("Division: " + result4);
+    }
+}
